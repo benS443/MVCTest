5c7c1e4c50617fc4f8484fd43895c9e3893dfe4f
benn443 <bbstolle@ualberta.ca>
2025-11-05 14:04:42 -0700

    Updated Project

--- MVCTest/Controllers/HomeController.cs
+++ MVCTest/Controllers/HomeController.cs
@@ -8,9 +8,12 @@ namespace MVCTest.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ExpensesDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ExpensesDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,9 +21,28 @@ namespace MVCTest.Controllers
             return View();
         }
 
+        public IActionResult Expenses()
+        {
+            var allExpenses = _context.Expenses.ToList();
+            return View(allExpenses);
+        }
+
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            _context.Expenses.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- MVCTest/Program.cs
+++ MVCTest/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MVCTest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ExpensesDbContext>(options =>
+    options.UseInMemoryDatabase("ExpensesDbContext"));
 
 var app = builder.Build();
 
